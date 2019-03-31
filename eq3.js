3b3456fff81d2371e08dc815d95ccee3fdb3eeb0
Jan VdA <jan.vandenaudenaerde@gmail.com>
2019-03-31 12:02:25 +0200

    disabled connect and setup

--- eq3.js
+++ eq3.js
@@ -99,11 +99,12 @@ module.exports = function(RED) {
         })
       }
       // see also https://github.com/noble/noble-device#usage
+      /*
       else if(!node.device.connectedAndSetUp) {
         RED.log.info("connectAndSetup device " + config.eq3device)
         node.device.connectAndSetup()
         .then(() => node.setCommand())
-      } else
+      } else */
         node.setCommand()
     });
   }
